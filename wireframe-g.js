1449cddbf65af387cbc67a9f64495870693ba324
sambiner <37849234+sambiner@users.noreply.github.com>
2019-01-28 14:33:40 -0800

    Add files via upload

--- wireframe-g.js
+++ wireframe-g.js
@@ -5,16 +5,24 @@ function removeColors(el){
 	document.getElementById("oof").src = "images/oof2.png";
 	document.getElementById("bigger").classList.remove("pink");
 	document.getElementById("image").src = "images/oof1.png";
+	document.getElementById("no").src = "";
 	var audio = document.getElementById("audiowoo");
 	audio.play();
-	
 }
 
-function addColors(el){
+function addStuff(el){
 	document.getElementById("small1").classList.add("blue");
 	document.getElementById("small2").classList.add("yellow");
 	document.getElementById("big").classList.add("green");
 	document.getElementById("oof").src = "images/oof.jpg";
 	document.getElementById("bigger").classList.add("pink");
 	document.getElementById("image").src = "";
+	document.getElementById("no").src = "images/oof1.png";
+	var audio = document.getElementById("audioof");
+	audio.play();
+}
+
+function no(el){
+	var audio = document.getElementById("bruh");
+	audio.play();
 }
